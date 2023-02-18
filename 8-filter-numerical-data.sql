57419e7af8afec6e0168b56bb006701a15c68819
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2023-02-18 19:46:11 +0530

    Update 8-filter-numerical-data.sql

--- 8-filter-numerical-data.sql
+++ 8-filter-numerical-data.sql
@@ -1,5 +1,7 @@
 -- Write a query to retrieve sales information for product ID 392 where more than 1 item has been sold in each sale. 
 
-SELECT sales_id, delivery_order_id,unit_price, quantity, total_price 
+SELECT *
 FROM sales 
-WHERE product_id=392;
+WHERE product_id=392 and quantity>1;
+
+
