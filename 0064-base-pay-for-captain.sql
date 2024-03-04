253bcfd43370579dbc2920a82908c7aa4d71ab95
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2024-03-04 23:45:52 +0530

    Create 0064-base-pay-for-captain.sql

---
+++ 0064-base-pay-for-captain.sql
@@ -0,0 +1,5 @@
+-- Find the base pay for Police Captains.
+-- Output the employee name along with the corresponding base pay.
+
+select employeename, basepay from sf_public_salaries where jobtitle = 'CAPTAIN III (POLICE DEPARTMENT)';
+
