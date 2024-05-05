7ee61ae725c015387606d7ffb97ac41472b4d85e
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2024-05-05 20:29:34 +0530

    Create 0086-meta-highest-energy-consumption.sql

---
+++ 0086-meta-highest-energy-consumption.sql
@@ -0,0 +1,22 @@
+-- Find the date with the highest total energy consumption from the Meta/Facebook data centers. Output the date along with the total energy consumption across all data centers.
+
+-- unions over join for certain cases
+WITH total_energy AS
+  (
+  SELECT *
+   FROM fb_eu_energy eu
+   UNION ALL SELECT *
+   FROM fb_asia_energy asia
+   UNION ALL SELECT *
+   FROM fb_na_energy na),
+energy_by_date AS
+  (SELECT date, sum(consumption) AS total_energy
+   FROM total_energy
+   GROUP BY date
+   ORDER BY date ASC),
+max_energy AS
+  (SELECT max(total_energy) AS max_energy
+   FROM energy_by_date)
+SELECT ebd.date,ebd.total_energy
+FROM energy_by_date ebd
+JOIN max_energy me ON ebd.total_energy = me.max_energy
