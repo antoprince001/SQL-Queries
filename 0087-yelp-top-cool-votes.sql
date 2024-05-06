3e96e0f2ba2048039287d17a6bd0b09fd5141da2
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2024-05-07 00:09:23 +0530

    Create 0087-yelp-top-cool-votes.sql

---
+++ 0087-yelp-top-cool-votes.sql
@@ -0,0 +1,6 @@
+-- Find the review_text that received the highest number of  'cool' votes.
+-- Output the business name along with the review text with the highest numbef of 'cool' votes.
+
+select business_name, review_text 
+from yelp_reviews 
+where cool = (select max(cool) from yelp_reviews);
