e3a88284dfb3b31a4880945e98c64e9bbbda3484
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2024-08-01 01:00:18 +0530

    Update 0099-twitter-highest-salary-in-department.sql

--- 0099-twitter-highest-salary-in-department.sql
+++ 0099-twitter-highest-salary-in-department.sql
@@ -6,3 +6,12 @@ select department, first_name, salary  from
 employee where (department, salary) in
 (select department, max(salary) as max_salary from employee
 group by department)  
+
+SELECT dept_salary.department, e.first_name, dept_salary.max_salary 
+FROM 
+    (SELECT department, max(salary) AS max_salary 
+     FROM employee
+     GROUP BY department) dept_salary 
+JOIN employee e
+ON e.department = dept_salary.department
+AND e.salary = dept_salary.max_salary;
