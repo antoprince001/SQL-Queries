07dea77c3b4110a2b9501b2e11ca9ac241f5458a
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2024-12-26 22:57:12 +0530

    Create challenge_1.sql

---
+++ advent_of_sql/challenge_1.sql
@@ -0,0 +1,6 @@
+-- Create a report that helps Santa and the elves understand:
+
+-- Each child's primary and backup gift choices
+-- Their color preferences
+-- How complex each gift is to make
+-- Which workshop department should handle the creation
