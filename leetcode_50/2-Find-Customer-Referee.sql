ebc1c4c1bfd24855c71108e58ebb33a2db70b621
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2025-12-04 20:47:24 +0530

    Create 2-Find-Customer-Referee.sql

---
+++ leetcode_50/2-Find-Customer-Referee.sql
@@ -0,0 +1,8 @@
+-- Find the names of the customer that are either:
+
+-- referred by any customer with id != 2.
+-- not referred by any customer.
+-- Return the result table in any order.
+
+SELECT name FROM Customer
+WHERE referee_id is null or referee_id != 2
